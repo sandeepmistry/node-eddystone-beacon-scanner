09240a5489b6bfb3019a854b8495a1375027d7ea
Christopher <hello@blick-labs.com>
2015-08-29 16:55:46 -0700

    Update basic example

--- examples/basic.js
+++ examples/basic.js
@@ -1,13 +1,11 @@
-var EddystoneBeaconScanner = require('../'); // use require('uri-beacon-scanner'), if installed from npm
+var EddystoneBeaconScanner = require('../'); // use require('eddystone-beacon-scanner'), if installed from npm
 
-EddystoneBeaconScanner.on('didFindBeacon', function(uriBeacon) {
-  console.log('discovered Eddystone Beacon:');
-  console.log(uriBeacon);
+EddystoneBeaconScanner.on('didFindBeacon', function(beacon) {
+  console.log('discovered Eddystone Beacon:\n', beacon);
 });
 
-EddystoneBeaconScanner.on('didLoseBeacon', function(uriBeacon) {
-  console.log('Lost Eddystone Beacon:');
-  console.log(uriBeacon);
+EddystoneBeaconScanner.on('didLoseBeacon', function(beacon) {
+  console.log('Lost Eddystone Beacon:\n', beacon);
 });
 
 EddystoneBeaconScanner.startScanning(true);

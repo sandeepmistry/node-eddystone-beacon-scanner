82a25cabf7b7be86f99fac15d02a67117526d59c
Christopher <hello@blick-labs.com>
2015-08-29 13:31:40 -0700

    Update didLoseBeacon listener. Add general beacon parser to handle individual frame types.

--- examples/basic.js
+++ examples/basic.js
@@ -1,7 +1,12 @@
 var EddystoneBeaconScanner = require('../'); // use require('uri-beacon-scanner'), if installed from npm
 
-EddystoneBeaconScanner.on('discover', function(uriBeacon) {
+EddystoneBeaconScanner.on('didFindBeacon', function(uriBeacon) {
   console.log('discovered Eddystone Beacon:');
+  console.log(uriBeacon);
+});
+
+EddystoneBeaconScanner.on('didLoseBeacon', function(uriBeacon) {
+  console.log('Lost Eddystone Beacon:');
   console.log(uriBeacon);
 });
 
